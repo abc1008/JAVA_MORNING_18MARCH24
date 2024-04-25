0e343c4e61c6628c59192bb1bd537b1522cef7ce
Aditya <ag89111@gmail.com>
2024-04-25 09:07:37 +0530

    Locators - name, classname, xpath
    login and logout in site

--- SeleniumBasics/src/selenium_basics_pack1/LaunchBrowser.java
+++ SeleniumBasics/src/selenium_basics_pack1/LaunchBrowser.java
@@ -5,75 +5,61 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class LaunchBrowser {
-	
-	
-	public static void main(String[] args) throws InterruptedException {
-		
-		
+public class LaunchBrowser
+{
+
+	public static void main(String[] args) throws InterruptedException
+	{
+
 		String browserName = "Chrome";
 		WebDriver driver;
-		
+
 //		WebDriver driver = new ChromeDriver(); 
-		
-		
-		if(browserName.equals("Chrome"))
+
+		if (browserName.equals("Chrome"))
 		{
-			 driver = new ChromeDriver(); 						// launch chrome browser
+			driver = new ChromeDriver(); // launch chrome browser
 		}
 		else
 		{
-			 driver = new EdgeDriver();
+			driver = new EdgeDriver();
 		}
-		
-		
-		
-		
-		
-		
+
 //		 Basic Commands in selenium:
-		
-		
-		driver.manage().window().maximize();        // maximize the browser
-		driver.get("https://www.google.com");		// hit URL
-		
+
+		driver.manage().window().maximize(); // maximize the browser
+		driver.get("https://www.google.com"); // hit URL
+
 		driver.navigate().to("https://www.facebook.com");
-		
+
 		Thread.sleep(2000);
-		
+
 		driver.navigate().back();
-		
+
 		Thread.sleep(2000);
 		driver.navigate().forward();
-		
+
 		Thread.sleep(2000);
 		driver.navigate().refresh();
-		
-		String title = driver.getTitle();   // title of the site
-		
+
+		String title = driver.getTitle(); // title of the site
+
 		System.out.println(title);
-		
-		String currentURL = driver.getCurrentUrl();    // returns the url of current browser
+
+		String currentURL = driver.getCurrentUrl(); // returns the url of current browser
 		System.out.println(currentURL);
-		
-		
+
 		// to verify if title contains text "sign up"
-		
-		if(title.contains("sign up"))
+
+		if (title.contains("sign up"))
 		{
 			System.out.println("Test Case Pass.");
 		}
-		else 
+		else
 		{
 			System.out.println("Test Case Fail.");
 		}
-		
-		
-		
-		
-		
-		
-		
+
 	}
-	
+
 }
--- SeleniumBasics/src/selenium_basics_pack1/Locators.java
+++ SeleniumBasics/src/selenium_basics_pack1/Locators.java
@@ -5,36 +5,47 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Locators {
-	
-	
-	public static void main(String[] args) throws InterruptedException {
-		
+public class Locators
+{
+	public static void main(String[] args) throws InterruptedException
+	{
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
-		
+
 		Thread.sleep(2000);
+
+		// ************ LOGIN *************
 		
 		// finding element on UI and storing it inside a variable
 		WebElement emailTextBox = driver.findElement(By.id("input-email"));
-		
+
 		Thread.sleep(2000);
-		
+
 		// enter data in a webelement located above
 		emailTextBox.sendKeys("ag89111@gmail.com");
+
+		Thread.sleep(2000);
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("Login@123");
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[contains(@class,'btn-primary')]")).click();
+	
+		
+		Thread.sleep(5000);
+		
+	
+		// ************ LOGOUT *************
+		// My Account Click
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//a[text()='Logout'])[1]")).click();
 		
 		
 		
-		
-		
-		
-		
+	
 	}
-	
-	
-	
-	
-	
 
 }
